d31c13ef39d76121c3281f587d2dbfa1fe25294f
beabebaw <51974257+beabebaw@users.noreply.github.com>
2021-08-23 10:07:23 -0300

    Create request-access-token.cs
    Request an access token with username and password input

---
+++ request-access-token.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Text.Json;
+
+// POST request to https://gene.lacuna.cc/api/users/login. The data sent is in JSON format 
+
+var url = "https://gene.lacuna.cc/api/users/login";
+
+// the request method is set to POST
+var request = WebRequest.Create(url);
+request.Method = "POST";
+
+// serialize an user object to JSON and transform it into an array of bytes 
+var user = new User("JohnDoe", "jardineiro94");
+var json = JsonSerializer.Serialize(user);
+byte[] byteArray = Encoding.UTF8.GetBytes(json);
+
+request.ContentType = "application/json";
+request.ContentLength = byteArray.Length;
+
+// get the stream of the request with GetRequestStream and write the byte array into the stream with Write
+using var reqStream = request.GetRequestStream();
+reqStream.Write(byteArray, 0, byteArray.Length);
+
+// get responde with GetResponse
+using var response = request.GetResponse();
+// and print the status of the response
+Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+// read the data from the response stream
+using var respStream = response.GetResponseStream();
+
+using var reader = new StreamReader(respStream);
+string data = reader.ReadToEnd();
+Console.WriteLine(data);
+
+record User(string username, string password);
