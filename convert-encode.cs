aaa71e844712fa46cec52ef6b6114552ebeaa8a8
beabebaw <51974257+beabebaw@users.noreply.github.com>
2021-08-27 18:18:37 -0300

    Update convert-encode.cs
    Some d*** sh** that I'm putting here

--- convert-encode.cs
+++ convert-encode.cs
@@ -13,6 +13,18 @@ TASK --> preciso de alguma coisa que converta bytes para 2-bits
 o objetivo é receber string e converter para base64
 */
 
+/*
+-> 2-bits arrays
+A: 0b00      C: 0b01
+T: 0b11      G: 0b10
+String:   "CATCGTCAGGAC"
+Bits:     0b010011011011010010100001
+Byte[]:   [0x4D, 0xB4, 0xA1] // notice the bits to byte conversion is Big-Endian
+Base64:   "TbSh"
+TASK --> preciso de alguma coisa que converta bytes para 2-bits
+o objetivo é receber string e converter para base64
+*/
+
 using System;
 using System.Text;
 
@@ -20,29 +32,42 @@ public class Example
 {
    public static void Main()
    {
-	   
-       // Define a byte array.
-       byte[] bytes1 = {0x4D, 0xB4, 0xA1};
-       Console.WriteLine("The byte array: ");
-       Console.WriteLine("   {0}\n", BitConverter.ToString(bytes1));
+	// BYTE ARRAYS
+	byte[] bigEndian = {0x4D, 0xB4, 0xA1};
+	byte[] decBytes = {67, 65, 84, 67, 71, 84, 67, 65, 71, 71, 65, 67};
+	byte[] hexBytes = {43, 41, 54, 43, 47, 54, 43, 41, 47, 47, 41, 43};
 
-       // Convert the array to a base 64 string.
-       string string1 = Convert.ToBase64String(bytes1);
-       Console.WriteLine("The base 64 string:\n   {0}\n", string1);
+	Console.WriteLine("Big endian array: ");
+	Console.WriteLine("   {0}\n", BitConverter.ToString(bigEndian));
+	Console.WriteLine("Decimal array: ");
+	Console.WriteLine("   {0}\n", BitConverter.ToString(decBytes));
+	Console.WriteLine("Hexadecimal array: ");
+	Console.WriteLine("   {0}\n", BitConverter.ToString(hexBytes));
 
-       // Convert a base 64 string to byte array.
-       byte[] bytes2 = Convert.FromBase64String(string1);
-       Console.WriteLine("The restored byte array: ");
-	   Console.WriteLine("   {0}\n", BitConverter.ToString(bytes2));
-	   
-	   // convert the byte array to a string
-	   byte[] newBytes = {67, 65, 84, 67, 71, 84, 67, 65, 71, 71, 65, 67};
-	   string string2 = Encoding.Default.GetString(newBytes);
-	   Console. WriteLine("The String is: \n   {0}\n", string2);
+	// BIG ENDIAN BYTE ARRAY TO BASE64
+	string string1 = Convert.ToBase64String(bigEndian);
+	Console.WriteLine("Big Endian to Base64:\n   {0}\n", string1);
 
-	   // convert the string to a byte array
-	   byte[] bytes3 = Encoding.Default.GetBytes(string2);
-	   Console. WriteLine("Byte Array is: \n   {0}\n", String.Join(" ", bytes3));
+	// BASE64 TO BIG ENDIAN BYTE ARRAY
+	byte[] bytes2 = Convert.FromBase64String(string1);
+	Console.WriteLine("Base64 to Big Endian: ");
+	Console.WriteLine("   {0}\n", BitConverter.ToString(bytes2));
+
+	// DECIMAL BYTE ARRAY TO STRING
+	string string2 = Encoding.Default.GetString(decBytes);
+	Console. WriteLine("Decimal byte array to String: \n   {0}\n", string2);
+
+	// STRING TO DECIMAL BYTE ARRAY
+	byte[] bytes3 = Encoding.Default.GetBytes(string2);
+	Console. WriteLine("String to Decimal byte array): \n   {0}\n", String.Join("-", bytes3));
+
+	// STRING TO HEXADECIMAL BYTE ARRAY
+	byte[] oi = Encoding.Unicode.GetBytes("CATCGTCAGGAC");
+	//Console.WriteLine("   {0}\n", BitConverter.ToString(oi));
+
+	byte[] tchau = Encoding.Convert(Encoding.Unicode, Encoding.ASCII, oi);
+	Console.WriteLine("String to Hexadecimal byte array: ");
+	Console.WriteLine("   {0}\n", BitConverter.ToString(tchau));
 	   
    }
 } 
@@ -70,9 +95,9 @@ class Program
 {
     static string ToBinaryString( Encoding encoding, string text )
     {
-		var binary01 = encoding.GetBytes( text ).Select( n => Convert.ToString( n, 2 ).PadLeft( 8, '0' ) );
-		var binary02 = encoding.GetBytes( text );
-        return string.Join( " ", binary01);
+	var binary01 = encoding.GetBytes( text ).Select( n => Convert.ToString( n, 2 ).PadLeft( 8, '0' ) );
+	var binary02 = encoding.GetBytes( text );
+	return string.Join( " ", binary01);
     }
 
     static void Main( string[] args )
