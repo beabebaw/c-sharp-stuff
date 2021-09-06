725d720859758d7ba4f7a547b318b6959f14fe23
beabebaw <51974257+beabebaw@users.noreply.github.com>
2021-09-06 13:48:18 -0300

    Create main.cs
    provisory commented text

---
+++ main.cs
@@ -0,0 +1,32 @@
+/* this script will call all that previous functions that i made
+in this order:
+
+- create user
+create username, password and email
+keep username and password
+
+- request token
+use the username and password
+keep the token
+
+- use access token to request job
+use the token
+keep the job id
+keep the string that will be worked on
+
+in no particular order:
+
+- encode string
+use the string
+send the id job
+send the result
+
+- decode base64
+use the string
+send the id job
+send the result
+
+- check gene
+use the string
+send the id job
+send the result */
