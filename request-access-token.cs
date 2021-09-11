020ac982dfaec1b467f70f662e64eb47c6cd446a
beabebaw <51974257+beabebaw@users.noreply.github.com>
2021-09-11 10:05:02 -0300

    Update request-access-token.cs
    as a function and a user dictionary

--- request-access-token.cs
+++ request-access-token.cs
@@ -3,41 +3,50 @@ using System.IO;
 using System.Text;
 using System.Net;
 using System.Text.Json;
+using System.Collections.Generic;
 
-// POST request to https://gene.lacuna.cc/api/users/login. The data sent is in JSON format 
+// POST request to https://gene.lacuna.cc/api/users/create. The data sent is in JSON format 
 
-var url = "https://gene.lacuna.cc/api/users/login";
+public class HttpRequest
+{
+	public static void Main()
+	{
+		var url = "https://gene.lacuna.cc/api/users/create";
 
-// the request method is set to POST
-var request = WebRequest.Create(url);
-request.Method = "POST";
+		// the request method is set to POST
+		var request = WebRequest.Create(url);
+		request.Method = "POST";
 
-// serialize an user object to JSON and transform it into an array of bytes 
-var user = new User("JohnDoe", "jardineiro94");
-var json = JsonSerializer.Serialize(user);
-byte[] byteArray = Encoding.UTF8.GetBytes(json);
+		// serialize an user object to JSON and transform it into an array of bytes 
+		//var user = new User("JohnDoe", "johngardener@gmail.com", "jardineiro94");
+		var user = new Dictionary<string, string>(){
+			{"username", "JohnDoe"},
+			{"email", "johngardener@gmail.com"},
+			{"password", "jardineiro94"}
+		};
 
-request.ContentType = "application/json";
-request.ContentLength = byteArray.Length;
+		var json = JsonSerializer.Serialize(user);
+		byte[] byteArray = Encoding.UTF8.GetBytes(json);
 
-// get the stream of the request with GetRequestStream and write the byte array into the stream with Write
-using var reqStream = request.GetRequestStream();
-reqStream.Write(byteArray, 0, byteArray.Length);
+		request.ContentType = "application/json";
+		request.ContentLength = byteArray.Length;
 
-// get response with GetResponse
-using var response = request.GetResponse();
-// and print the status of the response
-Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+		// get the stream of the request with GetRequestStream and write the byte array into the stream with Write
+		using var reqStream = request.GetRequestStream();
+		reqStream.Write(byteArray, 0, byteArray.Length);
 
-// read the data from the response stream
-using var respStream = response.GetResponseStream();
+		// get response with GetResponse
+		using var response = request.GetResponse();
+		// and print the status of the response
+		Console.WriteLine(((HttpWebResponse)response).StatusDescription);
 
-using var reader = new StreamReader(respStream);
-string data = reader.ReadToEnd();
-Console.WriteLine(data);
+		// read the data from the response stream
+		using var respStream = response.GetResponseStream();
 
-// get the access token only
-string accessToken = data.Substring(16).Split('"')[0];
-Console.WriteLine(accessToken);
+		using var reader = new StreamReader(respStream);
+		string data = reader.ReadToEnd();
+		Console.WriteLine(data);
 
-record User(string username, string password);
+		//record User(string username, string email, string password);
+	}
+}
