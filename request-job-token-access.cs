25025487c5734d71c25ce2f011dff9eac1a7c840
beabebaw <51974257+beabebaw@users.noreply.github.com>
2021-09-11 19:14:48 -0300

    Update request-job-token-access.cs
    functionalized

--- request-job-token-access.cs
+++ request-job-token-access.cs
@@ -2,18 +2,30 @@ using System;
 using System.IO;
 using System.Net;
 
-var url = "https://gene.lacuna.cc/api/dna/jobs";
-var token = "3C801A614966D740B82C96F4439416C90EFAA463ED9D5C9047012E65245BBF5F6C835817387674FF19BD1CCF250AA364376D218E41FCF2D0EDE1998ACB82B781CA06B6060EEAF1E31829EE4D64A0FEA2";
+var accessToken = "2DFB5994E9E9344BACFB44ED65AA5735876914D1258A1190CB207465530276DD0AEA98A1D79E824650AE42220FB502E7187B8A7E8091DB1D508F50A0ECA695D004DB60CD71D1AE440A0A366F99963BD9";
 
-var httpRequest = (HttpWebRequest)WebRequest.Create(url);
-httpRequest.Method = "GET";
+HttpRequest.Main(accessToken);
 
-httpRequest.Accept = "application/json";
-httpRequest.Headers["Authorization"] = "Bearer " + token;
+public class HttpRequest
+{
+	public static void Main(string accessToken)
+	{
+		var url = "https://gene.lacuna.cc/api/dna/jobs";
 
-var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-var streamReader = new StreamReader(httpResponse.GetResponseStream());
-var result = streamReader.ReadToEnd();
+		var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+		httpRequest.Method = "GET";
 
-Console.WriteLine(httpResponse.StatusCode);
-Console.WriteLine(result);
+		httpRequest.Accept = "application/json";
+		httpRequest.Headers["Authorization"] = "Bearer " + accessToken;
+
+		var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+		var reader = new StreamReader(httpResponse.GetResponseStream());
+		var data = reader.ReadToEnd();
+
+		Console.WriteLine(httpResponse.StatusCode);
+		Console.WriteLine(data);
+		
+		reader.Close();
+		httpResponse.Close();
+	}
+}
