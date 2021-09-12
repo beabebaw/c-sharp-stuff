5e957e04a6d6324e5f86697f74b0fa29ba85f10e
beabebaw <51974257+beabebaw@users.noreply.github.com>
2021-09-12 16:23:18 -0300

    Update response.cs

--- response.cs
+++ response.cs
@@ -5,9 +5,9 @@ using System.Net;
 using System.Text.Json;
 using System.Collections.Generic;
 
-string encode = "";
+string decode = "";
 
-Console.WriteLine(HttpRequest.Login(encode));
+Console.WriteLine(HttpRequest.Login(decode));
 
 // POST request to https://gene.lacuna.cc/api/users/login. The data sent is in JSON format
 // https://gene.lacuna.cc/api/dna/jobs/{id}/encode
@@ -15,11 +15,11 @@ Console.WriteLine(HttpRequest.Login(encode));
 // https://gene.lacuna.cc/api/dna/jobs/{id}/gene
 public class HttpRequest
 {
-	public static string Login(string encode)
+	public static string Login(string decode)
 	{
-		var accessToken = "D0CC6BC2DCCFCE4D8693814F1BD287499232834DACCA787F9D54251F89DAEF05B03CD48BF1FBB81AE7DA72373B9EBA297B714BCD226E94A7B6C46CC6FBBE332A62ACDB89ECA2B295C2898833AD547922";
-		var id = "6606af9e9c594374b22cfbc08fcf800e";
-		var url = "https://gene.lacuna.cc/api/dna/jobs/" + id + "/encode";
+		var accessToken = "";
+		var id = "";
+		var url = "https://gene.lacuna.cc/api/dna/jobs/" + id + "/decode";
 		
 		// the request method is set to POST
 		var request = WebRequest.Create(url);
@@ -27,7 +27,7 @@ public class HttpRequest
 
 		// serialize an user object to JSON and transform it into an array of bytes 
 		//var user = new User("JohnDoe", "jardineiro94");
-		var json = JsonSerializer.Serialize(encode);
+		var json = JsonSerializer.Serialize(decode);
 		byte[] byteArray = Encoding.UTF8.GetBytes(json);
 
 		request.ContentType = "application/json";
